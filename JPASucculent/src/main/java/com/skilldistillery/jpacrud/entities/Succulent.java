685a081b6c47ef1547a6185b066cde66fb214539
Josiah Candler <josiahcandler@Josiahs-MacBook-Pro.local>
2020-10-23 18:23:47 -0600

    Add Succulent unit test

--- JPASucculent/src/main/java/com/skilldistillery/jpacrud/entities/Succulent.java
+++ JPASucculent/src/main/java/com/skilldistillery/jpacrud/entities/Succulent.java
@@ -1,5 +1,45 @@
 package com.skilldistillery.jpacrud.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Succulent {
+	
+	public String getCommonName() {
+		return commonName;
+	}
+
+	public void setCommonName(String commonName) {
+		this.commonName = commonName;
+	}
+
+	@Override
+	public String toString() {
+		return "Succulent [id=" + id + ", commonName=" + commonName + "]";
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="common_name")
+	private String commonName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public Succulent() {
+		super();
+	}
 
 }
