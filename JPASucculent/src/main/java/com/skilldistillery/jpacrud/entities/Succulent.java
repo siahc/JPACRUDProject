8009cafdb032527f0f1fae9b0e5171b85b314bd5
Josiah Candler <josiahcandler@Josiahs-MacBook-Pro.local>
2020-10-25 00:52:48 -0600

    Update data model

--- JPASucculent/src/main/java/com/skilldistillery/jpacrud/entities/Succulent.java
+++ JPASucculent/src/main/java/com/skilldistillery/jpacrud/entities/Succulent.java
@@ -5,41 +5,105 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 public class Succulent {
-	
-	public String getCommonName() {
-		return commonName;
-	}
-
-	public void setCommonName(String commonName) {
-		this.commonName = commonName;
-	}
-
-	@Override
-	public String toString() {
-		return "Succulent [id=" + id + ", commonName=" + commonName + "]";
-	}
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	@Column(name="common_name")
 	private String commonName;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
+	
+	@Column(name="scientific_name")
+	private String scientificName;
+	
+	private String description;
+	
+	private Double price;
+	
+	
+	@Column(name="watering_needs")
+	private String wateringNeeds;
 
 
 	public Succulent() {
 		super();
 	}
 
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getCommonName() {
+		return commonName;
+	}
+
+
+	public void setCommonName(String commonName) {
+		this.commonName = commonName;
+	}
+
+
+	public String getScientificName() {
+		return scientificName;
+	}
+
+
+	public void setScientificName(String scientificName) {
+		this.scientificName = scientificName;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public Double getPrice() {
+		return price;
+	}
+	public String formatPrice() {
+		return String.format("$%.2f",price);
+	}
+
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+
+	public String getWateringNeeds() {
+		return wateringNeeds;
+	}
+
+
+	public void setWateringNeeds(String wateringNeeds) {
+		this.wateringNeeds = wateringNeeds;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Succulent [id=" + id + ", commonName=" + commonName + ", scientificName=" + scientificName
+				+ ", description=" + description + ", price=" + price + ", wateringNeeds=" + wateringNeeds + "]";
+	}
+	
+	
+	
+	
+	
 }
--- JPASucculent/src/test/java/com/skilldistillery/jpacrud/entities/SucculentTest.java
+++ JPASucculent/src/test/java/com/skilldistillery/jpacrud/entities/SucculentTest.java
@@ -1,7 +1,7 @@
 package com.skilldistillery.jpacrud.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -11,15 +11,27 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class SucculentTest {
 	
 	private static EntityManagerFactory emf;
-	
 	private EntityManager em;
 	private Succulent succulent;
 
+	@Test
+	@DisplayName("test Succulent entity mappings")
+	void test1() {
+	assertNotNull(succulent);
+	assertEquals("aloe", succulent.getCommonName());
+	assertEquals("aloe_vera", succulent.getScientificName());
+	assertEquals("Long, triangular-leaves; typically with soft thorns.", succulent.getDescription());
+	// ***PRICE SHOULD BE A DOUBLE NOT A STRING*** (it was ".getprice(String) in autocomplete for some reason
+	assertEquals("5.0", succulent.getPrice());
+	assertEquals("medium", succulent.getWateringNeeds());
+	}
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		emf = Persistence.createEntityManagerFactory("SucculentPU");
@@ -41,10 +53,10 @@ class SucculentTest {
 		em.close();
 	}
 
-	@Test
-	void test_Succulent_entity_mapping() {
-		assertNotNull(succulent);
-		assertEquals("Aloe", succulent.getCommonName());
-	}
+//	@Test
+//	void test_Succulent_entity_mapping() {
+//		assertNotNull(succulent);
+//		assertEquals("Aloe", succulent.getCommonName());
+//	}
 
 }
